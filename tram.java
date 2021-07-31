26ca35a858baf14845709a09542bf75086a61669
Daniel-Cas <dmattcomics@gmail.com>
2021-07-30 22:33:50 -0500

    Add exercise

---
+++ tram.java
@@ -0,0 +1,34 @@
+ 
+import java.util.Scanner;
+
+public class tram{
+
+
+public static void main( String args[]){
+
+
+	Scanner entrada = new Scanner(System.in);
+	
+	
+	int length = 0;
+
+	length = entrada.nextInt();
+
+	int input = new int[length][2];
+
+
+
+
+
+
+
+
+
+
+	}
+
+
+}
+
+
+
